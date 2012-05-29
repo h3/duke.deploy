24da6691d5efe4399f6148df04e08c2b60fe744f
Your Name <haineault@gmail.com>
2012-05-28 23:48:16 -0400

    Added folder structure, updated setup.py

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ extra = {}
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
-setup(name='mr.developer',
+setup(name='duke.deploy',
       version=version,
       description="A zc.buildout extension to ease the deployment of django projects.",
-      long_description=open("README.rst").read()#+ "\n\n" +
+      long_description=open("README.rst").read(),#+ "\n\n" +
                       #open(os.path.join("docs", "HELP.txt")).read() +
                       #open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -37,20 +37,20 @@ setup(name='mr.developer',
       keywords='',
       author='Maxime Haineault',
       author_email='max@motion-m.ca',
-      url='https://h3@github.com/h3/duke-deploy-django.git',
+      url='https://github.com/h3/duke.deploy',
       license='BSD',
-      packages=['duke', 'duke.deploydjango'],
+      packages=['duke', 'duke.deploy'],
       package_dir = {'': 'src'},
-      namespace_packages=['duke.deploydjango'],
+      namespace_packages=['duke.deploy'],
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
      #test_suite='dukedeploydjango.tests',
       entry_points="""
       [console_scripts]
-      develop = duke.deploydjango.deploy:deploy
+      develop = duke.deploy.deploy:deploy
       [zc.buildout.extension]
-      default = duke.deploydjango.extension:extension
+      default = duke.deploy.extension:extension
       """,
       **extra
       )
